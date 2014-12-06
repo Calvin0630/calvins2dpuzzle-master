a56bcad8224af193beed71cae75ff89935659f88
Calvin0630 <cg7057705@gmail.com>
2014-12-06 13:22:48 -0500

    I meant to get a haircut

--- Assets/Scripts/Player2Script.cs
+++ Assets/Scripts/Player2Script.cs
@@ -8,7 +8,7 @@ public class Player2Script : MonoBehaviour {
 	bool jumping2 = false;
 	float distanceFromGround;
 	float sprinting = 1;
-	public static float lightDelay = 150;
+	public static float lightDelay = 20;
 	float lightTimer = lightDelay;
 	// Use this for initialization
 	void Start () {
@@ -42,19 +42,25 @@ public class Player2Script : MonoBehaviour {
 
 
 
-		lightTimer += Time.time;
-		Debug.Log (lightTimer);
+
+		//controls for shooting light
 		if (Input.GetButton ("RB") && ((Input.GetAxis ("RX") + Input.GetAxis ("RY") > 0) || (Input.GetAxis ("RX") + Input.GetAxis ("RY") < 0)) && lightTimer > lightDelay ) {
 			GameObject prefab = (GameObject)Resources.Load ("ballOfLight");
 			GameObject light = (GameObject)Instantiate (prefab, transform.position, Quaternion.identity);
-			light.rigidbody2D.AddForce (new Vector2(Input.GetAxis ("RX"), Input.GetAxis ("RY")));
+			Vector2 direction = new Vector2(Input.GetAxis ("RX"), Input.GetAxis ("RY"));
+			light.rigidbody2D.AddForce (setMagnitude(.5f,direction));
 			lightTimer = 0;
 		}
+
+
 
 
 	}
 	
 	void FixedUpdate(){
+
+		lightTimer += 1;
+
 		Vector2 feet = new Vector2 (transform.position.x, transform.position.y-1.8f);
 		RaycastHit2D hit = Physics2D.Raycast (feet, -Vector2.up);
 		if (hit.collider != null)
@@ -67,12 +73,28 @@ public class Player2Script : MonoBehaviour {
 			jumping2 = false;
 		}
 
-		
+
+	}
+
+	public float getMagnitude(Vector2 vector) {
+		float magnitude;
+		magnitude = Mathf.Sqrt(vector.x*vector.x + vector.y*vector.y);
+		return magnitude;
 	}
 
 	public Vector2 setMagnitude(float magnitude, Vector2 vector) {
-		Vector2 result = new Vector2(0,0);
+		float ratio;
+		ratio = Mathf.Sqrt(magnitude*magnitude/(vector.x*vector.x + vector.y*vector.y));
+		Vector2 result = new Vector2(vector.x*ratio , vector.y*ratio);
 		return result;
+	}
+
+	public Vector3 getWorldMouseCoordinates() {
+		Vector3 mouse = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+		Vector3 mousePos = Camera.main.ScreenToViewportPoint (mouse);
+		Vector3 mousePos1 = new Vector3 (2f * (mousePos.x - .5f), 2f * (mousePos.y - .5f));
+		Vector3 mouseUnit = new Vector3 (Camera.main.aspect * Camera.main.orthographicSize * mousePos1.x, Camera.main.orthographicSize * mousePos1.y);
+		return mouseUnit;
 	}
 
 }
